529ec53ecb0da014ff03db9a3e1d5fec73c3aee3
Fellipe Augusto <62561359+Fellp-coffww@users.noreply.github.com>
2023-05-03 16:08:31 -0300

    Create TestMotorSTOP.vb

---
+++ TestMotorSTOP.vb
@@ -0,0 +1,309 @@
+VAR
+
+MotorTeste_ACmd_Liga : "EL-302".InterfacePLC.ACmd_Liga;				
+MotorTeste_ACmd_LigaDireto : "EL-302".InterfacePLC.ACmd_LigaDireto;				
+MotorTeste_ACmd_LigaReverso : "EL-302".InterfacePLC.ACmd_LigaReverso;				
+MotorTeste_ACmd_DesligaCsc : "EL-302".InterfacePLC.ACmd_DesligaCsc;				
+MotorTeste_ACmd_Bloqueia : "EL-302".InterfacePLC.ACmd_Bloqueia;				
+MotorTeste_MCmd_LigaDireto : "EL-302".InterfacePLC.MCmd_LigaDireto;				
+MotorTeste_MCmd_DesligaDireto : "EL-302".InterfacePLC.MCmd_DesligaDireto;				
+MotorTeste_MCmd_LigaReverso : "EL-302".InterfacePLC.MCmd_LigaReverso;				
+MotorTeste_MCmd_DesligaReverso : "EL-302".InterfacePLC.MCmd_DesligaReverso;				
+MotorTeste_MCmd_Liga : "EL-302".InterfacePLC.MCmd_Liga;				
+MotorTeste_MCmd_Desliga : "EL-302".InterfacePLC.MCmd_Desliga;				
+MotorTeste_MCmd_ResetAlarme : "EL-302".InterfacePLC.MCmd_ResetAlarme;				
+MotorTeste_MCmd_HabOperacional : "EL-302".InterfacePLC.MCmd_HabOperacional;				
+MotorTeste_Bloqueado : "EL-302".InterfacePLC.Bloqueado;				
+MotorTeste_ModoTeste : "EL-302".InterfacePLC.ModoTeste;				
+MotorTeste_Manual : "EL-302".InterfacePLC.Manual;				
+MotorTeste_Alarme : "EL-302".InterfacePLC.Alarme;				
+MotorTeste_SaidaReverso : "EL-302".InterfacePLC.SaidaReverso;				
+MotorTeste_Saida : "EL-302".InterfacePLC.Saida;				
+MotorTeste_SaidaDireto : "EL-302".InterfacePLC.SaidaDireto;				
+MotorTeste_Reversivel : "EL-302".InterfacePLC.Reversivel;				
+MotorTeste_Remoto : "EL-302".InterfacePLC.Remoto;				
+MotorTeste_Sentido : "EL-302".InterfacePLC.Sentido;				
+MotorTeste_FBDireto : "EL-302".InterfacePLC.FBDireto;				
+MotorTeste_FBReverso : "EL-302".InterfacePLC.FBReverso;				
+MotorTeste_FBBLoqueado : "EL-302".InterfacePLC.FBBLoqueado;				
+MotorTeste_FB : "EL-302".InterfacePLC.FB;				
+MotorTeste_Termico : "EL-302".InterfacePLC.Termico;				
+MotorTeste_Lockout : "EL-302".InterfacePLC.Lockout;				
+MotorTeste_Permissao : "EL-302".InterfacePLC.Permissao;				
+MotorTeste_Falha : "EL-302".InterfacePLC.Falha;				
+MotorTeste_Partindo : "EL-302".InterfacePLC.Partindo;				
+MotorTeste_Parando : "EL-302".InterfacePLC.Parando;				
+MotorTeste_InterlocksOK : "EL-302".InterfacePLC.InterlocksOK;				
+MotorTeste_PermissivosOK : "EL-302".InterfacePLC.PermissivosOK;				
+MotorTeste_DesalocMe : "EL-302".InterfacePLC.DesalocMe;				
+MotorTeste_FBLigado : "EL-302".InterfacePLC.FBLigado;				
+MotorTeste_Contaminador : "EL-302".InterfacePLC.Contaminador;				
+MotorTeste_Manutencao : "EL-302".InterfacePLC.Manutencao;				
+MotorTeste_ByPassActive : "EL-302".InterfacePLC.ByPassActive;				
+MotorTeste_BloqCarga : "EL-302".InterfacePLC.BloqCarga;				
+MotorTeste_BloqSensorMov : "EL-302".InterfacePLC.BloqSensorMov;				
+MotorTeste_BloqMaxPartidas : "EL-302".InterfacePLC.BloqMaxPartidas;				
+MotorTeste_ParadaControlada : "EL-302".InterfacePLC.ParadaControlada;				
+MotorTeste_ParadaImediata : "EL-302".InterfacePLC.ParadaImediata;				
+MotorTeste_STS1 : "EL-302".InterfacePLC.STS1;				
+MotorTeste_Estado : "EL-302".InterfacePLC.Estado;				
+MotorTeste_Idx : "EL-302".InterfacePLC.Idx;				
+MotorTeste_TipoPartida : "EL-302".InterfacePLC.TipoPartida;				
+MotorTeste_FreqEscrita : "EL-302".InterfacePLC.FreqEscrita;				
+MotorTeste_FreqLida : "EL-302".InterfacePLC.FreqLida;				
+MotorTeste_AlarmeTipo : "EL-302".InterfacePLC.AlarmeTipo;				
+MotorTeste_CorrenteLida : "EL-302".InterfacePLC.CorrenteLida;				
+MotorTeste_CorrenteNorminal : "EL-302".InterfacePLC.CorrenteNorminal;				
+MotorTeste_RPM_Minimo : "EL-302".InterfacePLC.RPM_Minimo;				
+MotorTeste_RPM_Maximo : "EL-302".InterfacePLC.RPM_Maximo;				
+MotorTeste_RPM : "EL-302".InterfacePLC.RPM;				
+MotorTeste_SaidaResetFalha : "EL-302".InterfacePLC.SaidaResetFalha;				
+MotorTeste_LigaDireto : "EL-302".InterfacePLC.LigaDireto;				
+MotorTeste_LigaReverso : "EL-302".InterfacePLC.LigaReverso;				
+MotorTeste_Liga : "EL-302".InterfacePLC.Liga;				
+MotorTeste_Pronto : "EL-302".InterfacePLC.Pronto;				
+MotorTeste_Prontorev : "EL-302".InterfacePLC.Prontorev;				
+MotorTeste_Simula : "EL-302".InterfacePLC.Simula;				
+MotorTeste_ProntoDir : "EL-302".InterfacePLC.ProntoDir;				
+MotorTeste_CascLigada : "EL-302".InterfacePLC.CascLigada;				
+MotorTeste_CascDesligada : "EL-302".InterfacePLC.CascDesligada;				
+MotorTeste_Tempo_Esvaziamento : "EL-302".InterfacePLC.Tempo_Esvaziamento;				
+MotorTeste_Tempo_CascLigada : "EL-302".InterfacePLC.Tempo_CascLigada;				
+MotorTeste_Tempo_CascDesligada : "EL-302".InterfacePLC.Tempo_CascDesligada;				
+MotorTeste_Tempo_FalhaAcionamento : "EL-302".InterfacePLC.Tempo_FalhaAcionamento;				
+MotorTeste_Tempo_Teste : "EL-302".InterfacePLC.Tempo_Teste;				
+MotorTeste_Aux : "EL-302".InterfacePLC.Aux;				
+MotorTeste_ONS : "EL-302".InterfacePLC.ONS;				
+MotorTeste_CorrenteCapMin : "EL-302".InterfacePLC.CorrenteCapMin;				
+MotorTeste_CorrenteCapMax : "EL-302".InterfacePLC.CorrenteCapMax;				
+MotorTeste_CorrenteCapCarga : "EL-302".InterfacePLC.CorrenteCapCarga;				
+MotorTeste_CargaAlarme : "EL-302".InterfacePLC.CargaAlarme;				
+MotorTeste_CargaFalha : "EL-302".InterfacePLC.CargaFalha;				
+MotorTeste_TempMax : "EL-302".InterfacePLC.TempMax;				
+MotorTeste_ioSimula : "EL-302".ioSimula;				
+
+
+END_VAR
+
+STEP: "SIMULA SINAIS PARA MOTOR PARTIR"
+
+MotorTeste_IOSimula  := True; 
+MotorTeste_Manual := False; 
+MotorTeste_InterlocksOK := True;
+MotorTeste_PermissivosOK := True; 
+MotorTeste_Lockout := True; 
+MotorTeste_ModoTeste := False;
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_IOSimula, True);
+ASSERT.Equal(MotorTeste_Manual, False);
+ASSERT.Equal(MotorTeste_InterlocksOK, True);
+ASSERT.Equal(MotorTeste_PermissivosOK, True);
+ASSERT.Equal(MotorTeste_Pronto , True);
+ASSERT.NotEqual(MotorTeste_STS1,0);
+
+END_STEP
+
+
+STEP: "MOTOR PRONTO EM AUTO"
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Pronto, True);
+
+END_STEP
+
+STEP: "TENTA PARTIR EM AUTO // SAIDA"
+
+MotorTeste_ACmd_Liga := True;
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Saida, True);
+
+END_STEP
+
+STEP: "TENTA PARTIR EM AUTO // FB"
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_FB, True);
+
+END_STEP
+
+STEP: "INTERLOCK N/OK"
+
+MotorTeste_InterlocksOK := false; 
+RUN(CYCLES := 2);
+ASSERT.Equal(MotorTeste_Saida, false);
+ASSERT.Equal(MotorTeste_FB, false);
+
+END_STEP
+
+STEP: "SIMULA SINAIS PARA MOTOR PARTIR2"
+
+MotorTeste_IOSimula  := True; 
+MotorTeste_Manual := False; 
+MotorTeste_InterlocksOK := True;
+MotorTeste_PermissivosOK := True; 
+MotorTeste_Lockout := True; 
+MotorTeste_ModoTeste := False;
+MotorTeste_MCmd_ResetAlarme := True;
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_IOSimula, True);
+ASSERT.Equal(MotorTeste_Manual, False);
+ASSERT.Equal(MotorTeste_InterlocksOK, True);
+ASSERT.Equal(MotorTeste_PermissivosOK, True);
+ASSERT.Equal(MotorTeste_Pronto , True);
+ASSERT.NotEqual(MotorTeste_STS1,0);
+
+
+END_STEP
+
+STEP: "MOTOR PRONTO EM AUTO2"
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Pronto, True);
+
+END_STEP
+
+STEP: "TENTA PARTIR EM AUTO // SAIDA2"
+
+MotorTeste_ACmd_Liga := True;
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Saida, True);
+
+END_STEP
+
+STEP: "TENTA PARTIR EM AUTO // FB2"
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_FB, True);
+
+END_STEP
+
+
+STEP: "TERMICO"
+
+MotorTeste_Termico:= false; 
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Termico, false);
+
+END_STEP
+
+STEP: "TERMICO DESLIGA MOTOR"
+
+RUN(CYCLES := 2);
+ASSERT.Equal(MotorTeste_Saida, false);
+ASSERT.Equal(MotorTeste_FB, false);
+
+END_STEP
+
+STEP: "SIMULA SINAIS PARA MOTOR PARTIR3"
+
+MotorTeste_Termico := true;
+MotorTeste_IOSimula  := True; 
+MotorTeste_Manual := False; 
+MotorTeste_InterlocksOK := True;
+MotorTeste_PermissivosOK := True; 
+MotorTeste_Lockout := True; 
+MotorTeste_ModoTeste := False;
+MotorTeste_Alarme := false; 
+
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_IOSimula, True);
+ASSERT.Equal(MotorTeste_Manual, False);
+ASSERT.Equal(MotorTeste_InterlocksOK, True);
+ASSERT.Equal(MotorTeste_PermissivosOK, True);
+ASSERT.Equal(MotorTeste_Pronto , True);
+ASSERT.Equal(MotorTeste_Termico , True);
+ASSERT.Equal(MotorTeste_Alarme , false);
+ASSERT.NotEqual(MotorTeste_STS1,0);
+
+
+END_STEP
+
+STEP: "MOTOR PRONTO EM AUTO3"
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Pronto, True);
+
+END_STEP
+
+STEP: "TENTA PARTIR EM AUTO // SAIDA3"
+
+MotorTeste_ACmd_Liga := True;
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Saida, True);
+
+END_STEP
+
+STEP: "TENTA PARTIR EM AUTO // FB3"
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_FB, True);
+
+END_STEP
+
+STEP: "LOCKOUT"
+
+MotorTeste_lockout:= false; 
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Lockout, false);
+
+END_STEP
+
+STEP: "LOCKOUT DESLIGA MOTOR"
+
+RUN(CYCLES := 2);
+ASSERT.Equal(MotorTeste_Lockout, false);
+ASSERT.Equal(MotorTeste_Lockout, false);
+
+END_STEP
+
+STEP: "SIMULA SINAIS PARA MOTOR PARTIR4"
+
+MotorTeste_Termico := true;
+MotorTeste_IOSimula  := True; 
+MotorTeste_Manual := False; 
+MotorTeste_InterlocksOK := True;
+MotorTeste_PermissivosOK := True; 
+MotorTeste_Lockout := True; 
+MotorTeste_ModoTeste := False;
+MotorTeste_Alarme := false; 
+
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_IOSimula, True);
+ASSERT.Equal(MotorTeste_Manual, False);
+ASSERT.Equal(MotorTeste_InterlocksOK, True);
+ASSERT.Equal(MotorTeste_PermissivosOK, True);
+ASSERT.Equal(MotorTeste_Pronto , True);
+ASSERT.Equal(MotorTeste_Termico , True);
+ASSERT.Equal(MotorTeste_Alarme , false);
+ASSERT.NotEqual(MotorTeste_STS1,0);
+
+
+END_STEP
+
+STEP: "MOTOR PRONTO EM AUTO4"
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Pronto, True);
+
+END_STEP
+
+STEP: "TENTA PARTIR EM AUTO // SAIDA4"
+
+MotorTeste_ACmd_Liga := True;
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_Saida, True);
+
+END_STEP
+
+STEP: "TENTA PARTIR EM AUTO // FB4"
+
+RUN(CYCLES := 1);
+ASSERT.Equal(MotorTeste_FB, True);
+
+END_STEP
